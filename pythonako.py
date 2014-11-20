62f85628cbbb8ba998d32b274170ff642683e7b3
claudio-navarro-martinez <claudio.oami@gmail.com>
2014-11-20 12:45:57 +0000

    Update pythonako.py

--- pythonako.py
+++ pythonako.py
@@ -3,3 +3,15 @@ lista=[('a',1),('b',2),('c',3)]
 df=pd.DataFrame(lista,columns=['col1','col2'])
 
 df.groupby(['col1']).agg(['count','mean'])
+
+-- Query sample v$session y conseguimos los SQL que estan
+select /*+ ORDERED USE_NL(t) opt_param('_optimizer_sortmerge_join_enabled','false') opt_param('hash_join_enabled','false') NO_TRANSFORM_DISTINCT_AGG */ 
+            sqlid , count(*) "COUNT", count(distinct r.rn) DISTCOUNT
+        from
+            (select /*+ no_unnest */ rownum rn from dual connect by level <= 10000) r
+          , v$session t
+        where status = 'ACTIVE' and (wait_class != 'Idle' or state != 'WAITING')
+        group by
+            sqlid
+        order by
+            "COUNT" desc, sqlid
